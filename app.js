f86d7336481cafb51b5ddb4b3f743674392bf4b5
Manu-Estrada <poolbarmanu@hotmail.com>
2022-12-07 10:36:58 +0100

    finish

---
+++ app.js
@@ -0,0 +1,41 @@
+
+ console.log("hello")
+ const flour = "harina de fuerza";
+ const salt = "1 cuchara pequeña de sal fina";
+ const water = "2 tazas de agua tibia";
+ const oil = "2 cucharadas de aceite de oliva";
+ const yeast = "25 gramos de levadura fresca";
+ 
+ const tomato = "salsa de tomate";
+ const mozzarella = "mozzarella";
+ const cheese = "queso de cabra";
+ const onion = "cebolla caramelizada";
+ const bacon = "bacon";
+ 
+ function makeTheDough () {
+     
+ }
+ 
+ var pizza = "";
+ 
+ function app()
+ {
+     makePpizza();
+     deliverOrder(pizza);
+ }
+ 
+ console.log("mezclar 25 gramos de "+ yeast + "con dos tazas de agua tibia")
+ console.log("añadimos dos cucharadas de" + oil)
+ console.log("añadimos a un recipiente " + flour)
+ console.log("añadir una cuchara pequeña de" +salt)
+ console.log("amasar hasta que no se pegue en las manos")
+ console.log("darle forma a la masa para la base de una pizza")
+ console.log("dejar reposar la base durante 45 minutos") 
+ console.log("base con forma circular obtenida")
+ console.log("añadir salsa de "+ tomato +" hasta cubrir la base")
+ console.log("añadir "+ onion +" caramelizada al gusto")
+ console.log("añadir " + bacon +" al gusto")
+ console.log("añadir queso "+ mozzarella +" y de "+ cheese +" al gusto")
+ console.log("hornear la pizza durante 10 minutos a 180º") 
+ console.log("pizza lista para su consumo")
+ console.log("pizza entregada")
